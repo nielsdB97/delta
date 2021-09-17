90823427048f30d216585a92e04d06673c0320e4
Niels de Bruin <nielsdb97@gmail.com>
2021-09-17 11:20:13 +0200

    Add frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Heading, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <Stack>
+      <Heading>Hello World!</Heading>
+    </Stack>
+  );
+};
+
+export default Home;
