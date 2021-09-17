8c8f5f8da616c161655d345f783fcb981e42a30e
Niels de Bruin <nielsdb97@gmail.com>
2021-09-17 16:36:41 +0200

    Add amount badge to favorites Header button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,17 @@
-import { Stack, Heading, Text, IconButton } from "@chakra-ui/react";
+import { Box, Stack, Heading, Text, IconButton } from "@chakra-ui/react";
+import { FavoritesContext } from "context/FavoritesContext";
 import { FavoritesDrawerOpenContext } from "context/FavoritesDrawerOpenContext";
 import { useContext } from "react";
 import { HiHeart } from "react-icons/hi";
 
 function Header() {
   const FavoritesDrawerDisclosure = useContext(FavoritesDrawerOpenContext);
+  const useFavorites = useContext(FavoritesContext);
+
+  if (!useFavorites) {
+    return null;
+  }
+  const [favorites] = useFavorites;
 
   return (
     <Stack
@@ -20,11 +27,27 @@ function Header() {
       </Heading>
 
       <Stack isInline alignItems="center">
-        <IconButton
-          icon={<HiHeart />}
-          onClick={FavoritesDrawerDisclosure?.onToggle}
-          aria-label="Favorieten"
-        />
+        <Box pos="relative">
+          <IconButton
+            icon={<HiHeart />}
+            onClick={FavoritesDrawerDisclosure?.onToggle}
+            aria-label="Favorieten"
+          />
+          <Box
+            position="absolute"
+            top="-10px"
+            right="-10px"
+            w="6"
+            h="6"
+            bg="pink.500"
+            color="white"
+            rounded="full"
+            textAlign="center"
+            fontSize="sm"
+          >
+            {favorites.length}
+          </Box>
+        </Box>
         <Text as="span" color="white" fontWeight="semibold">
           Welkom Niels!
         </Text>
